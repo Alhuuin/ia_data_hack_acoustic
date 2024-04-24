44784f9d94df736f271e714f9fe6c37c0eee7c49
alicecariou <alice.cariou@epita.fr>
2024-04-24 16:45:48 +0000

    feat: vggish pretrained notebook done

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -2,7 +2,7 @@ import room_setup
 
 class Dataset(object):
     def __init__(self,
-                room_setup: roomsetup.RoomSetup,
+                room_setup: room_setup.RoomSetup,
                 preprocess_dir: str
                 ):
         self.room_setup = room_setup
